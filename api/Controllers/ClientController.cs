fd415ed7271e27b8d5259689a2320e231ae477de
ewaight12 <ewaight12@gmail.com>
2017-10-30 15:00:12 -0400

    Working Client Controller

--- api/Controllers/ClientController.cs
+++ api/Controllers/ClientController.cs
@@ -39,15 +39,15 @@ namespace api.Controllers {
         [Route ("")]
         [HttpPost]
         public IActionResult Create ([FromBody] Client client) {
-                if (client == null) {
-                    return BadRequest ();
-                }
-
-                _context.Clients.Add (client);
-                _context.SaveChanges ();
-
-                return CreatedAtRoute ("", new { id = client.Id }, client);
+            if (client == null) {
+                return BadRequest ();
             }
+
+            _context.Clients.Add (client);
+            _context.SaveChanges ();
+
+            return CreatedAtRoute ("", new { id = client.Id }, client);
+        }
 
         [Route ("{id}")]
         [HttpPut]
@@ -69,6 +69,19 @@ namespace api.Controllers {
             selectedClient.Password = client.Password;
 
             _context.Clients.Update (selectedClient);
+            _context.SaveChanges ();
+            return new NoContentResult ();
+        }
+
+        [Route ("{id}")]
+        [HttpDelete]
+        public IActionResult Delete (int id) {
+            var item = _context.Clients.FirstOrDefault (t => t.Id == id);
+            if (item == null) {
+                return NotFound ();
+            }
+
+            _context.Clients.Remove (item);
             _context.SaveChanges ();
             return new NoContentResult ();
         }
